02384e3d5a74bda482b4cf302ab8c473228a1b9d
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-01-25 00:05:50 +0530

    Update CountryController.cs

--- World_Project_First_Web_API/World_Project_First_Web_API/Controllers/CountryController.cs
+++ World_Project_First_Web_API/World_Project_First_Web_API/Controllers/CountryController.cs
@@ -34,5 +34,14 @@ namespace World_Project_First_Web_API.Controllers
             _dbContext.SaveChanges();
             return Ok();
         }
+
+         [HttpDelete("{id:int}")]
+         public ActionResult Delete(int id)
+         {
+             Country country = _dbContext.Countries.Find(id);
+             _dbContext.Countries.Remove(country);
+             _dbContext.SaveChanges();
+             return Ok();
+         }
     }
 }
