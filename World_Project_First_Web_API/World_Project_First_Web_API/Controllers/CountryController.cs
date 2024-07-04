77bff19661f9208406ae0d89f3b5111d007ae699
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-07-04 12:54:10 +0530

    Update CountryController.cs

--- World_Project_First_Web_API/World_Project_First_Web_API/Controllers/CountryController.cs
+++ World_Project_First_Web_API/World_Project_First_Web_API/Controllers/CountryController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using World_Project_First_Web_API.Data;
+using World_Project_First_Web_API.Models;
 
 namespace World_Project_First_Web_API.Controllers
 {
@@ -14,42 +15,90 @@ namespace World_Project_First_Web_API.Controllers
         {
             _dbContext = DbContext;
         }
-        
         [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(204)]
         public ActionResult<IEnumerable<Country>> GetAll()
         {
-            return _dbContext.Countries.ToList();
-        }
-        
-        [HttpGet("{id:int}")]
-        public ActionResult<Country> Get(int id)
-        {
-            return _dbContext.Countries.Find(id);
-        }
-        
-        [HttpPost]
-        public ActionResult<Country> Create([FromBody] Country country)
-        {
-            _dbContext.Countries.Add(country);
-            _dbContext.SaveChanges();
-            return Ok();
+            var countries = _dbContext.Countries.ToList();
+            if(countries == null)
+            {
+                return NoContent();
+            }
+            return Ok(countries);
         }
 
-        [HttpPut]
-        public ActionResult<Country> Update([FromBody] Country country)
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(204)]
+        public ActionResult<Country> Get(int id)
         {
-            _dbContext.Countries.Update(country);
-            _dbContext.SaveChanges();
-            return Ok();
+            var country = _dbContext.Countries.Find(id);
+            if(country == null)
+            {
+                return NoContent();
+            }
+            return Ok(country);
         }
-        
-         [HttpDelete("{id:int}")]
-         public ActionResult Delete(int id)
-         {
-             Country country = _dbContext.Countries.Find(id);
-             _dbContext.Countries.Remove(country);
-             _dbContext.SaveChanges();
-             return Ok();
-         }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(409)]
+        public ActionResult<Country> Create([FromBody] Country country)
+        {
+            bool uniqueName = _dbContext.Countries.AsQueryable().Where(x => x.name.ToLower().Trim() == country.name.ToLower().Trim()).Any();
+            if(uniqueName)
+            {
+                return Conflict("Country name should be unique");
+            }
+            _dbContext.Countries.Add(country);
+            _dbContext.SaveChanges();
+            return CreatedAtAction("GetById",new {id=country.id},country);
+        }
+
+        [HttpPut ("{id:int}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public ActionResult<Country> Update(int id,[FromBody] Country country)
+        {
+            if(id != country.id)
+            {
+                return BadRequest("Id mismatch");
+            }
+            var findCountry = _dbContext.Countries.Find(id);
+            if(findCountry == null)
+            {
+                return NotFound("The Id "+ id + " was not found");
+            }
+
+            findCountry.name = country.name;
+            findCountry.code = country.code;
+            findCountry.shortName = country.shortName;
+
+            _dbContext.Countries.Update(findCountry);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public ActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            Country country = _dbContext.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Countries.Remove(country);
+            _dbContext.SaveChanges();
+            return NoContent();
+        }
     }
 }
